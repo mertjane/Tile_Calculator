3435b07bc1cdc79f35db596c8b9c37b89b493ccb
unknown <mck0391@gmail.com>
2023-11-21 19:13:39 +0000

    initial commit

--- tile-calculator-script.js
+++ tile-calculator-script.js
@@ -1,50 +1,47 @@
 (function ($) {
-    $(document).ready(function () {
+    // Function to round a number to the nearest integer
+    function roundToInteger(number) {
+        return Math.round(number);
+    }
 
-        // Function to round a number to the nearest integer
-        function roundToInteger(number) {
-            return Math.round(number);
-        }
+    // Event handler for square meter input change
+    $('#squareMeterInput').on('input', function () {
+        updateCalculation();
+    });
 
-        // Event handler for square meter input change
-        $('#squareMeterInput').on('input', function () {
-            updateCalculation();
-        });
-
-        // Event handler for tile piece input change
-        $('#tilePieceInput').on('keydown', function (e) {
-            // Prevent entering decimals
-            if (e.key === '.' || e.key === ',') {
-                e.preventDefault();
-            }
-        });
-
-        // Event handler for tile piece input change
-        $('#tilePieceInput').on('input', function () {
-            updateCalculation();
-        });
-
-        // Event handler for WPC Variations Radio Buttons selected on click
-        $(document).on('woovr_selected_on_click', function (e, selected, variations, variations_form) {
-            updateCalculation();
-        });
-
-        // Event handler for WPC Variations Radio Buttons selected
-        $(document).on('woovr_selected', function (e, selected, variations, variations_form) {
-            updateCalculation();
-        });
-
-        // Function to update the calculation based on inputs
-        function updateCalculation() {
-            var squareMeterValue = parseFloat($('#squareMeterInput').val());
-            var tileSize = parseFloat($('input[name="' + wpc_vars.sizemm + '"]:checked').val());
-
-            if (!isNaN(squareMeterValue) && squareMeterValue > 0 && !isNaN(tileSize) && tileSize > 0) {
-                var tileQuantity = roundToInteger(squareMeterValue / ((tileSize / 1000) * (tileSize / 1000))); // Convert size to meters
-                $('#tilePieceInput').val(tileQuantity);
-            } else {
-                $('#tilePieceInput').val('');
-            }
+    // Event handler for tile piece input change
+    $('#tilePieceInput').on('keydown', function (e) {
+        // Prevent entering decimals
+        if (e.key === '.' || e.key === ',') {
+            e.preventDefault();
         }
     });
+
+    // Event handler for tile piece input change
+    $('#tilePieceInput').on('input', function () {
+        updateCalculation();
+    });
+
+    // Event handler for WPC Variations Radio Buttons selected on click
+    $(document).on('woovr_selected_on_click', function (e, selected, variations, variations_form) {
+        updateCalculation(wpc_vars);
+    });
+
+    // Event handler for WPC Variations Radio Buttons selected
+    $(document).on('woovr_selected', function (e, selected, variations, variations_form) {
+        updateCalculation(wpc_vars);
+    });
+
+    // Function to update the calculation based on inputs
+    function updateCalculation(wpc_vars) {
+        var squareMeterValue = parseFloat($('#squareMeterInput').val());
+        var tileSize = parseFloat($('input[name="' + wpc_vars.sizemm + '"]:checked').val());
+
+        if (!isNaN(squareMeterValue) && squareMeterValue > 0 && !isNaN(tileSize) && tileSize > 0) {
+            var tileQuantity = roundToInteger(squareMeterValue / ((tileSize / 1000) * (tileSize / 1000))); // Convert size to meters
+            $('#tilePieceInput').val(tileQuantity);
+        } else {
+            $('#tilePieceInput').val('');
+        }
+    }
 })(jQuery);
--- tile-calculator.php
+++ tile-calculator.php
@@ -1,30 +1,3 @@
-<?php
-/*
-Plugin Name: Tile Calculator
-Description: Tile calculator WordPress.
-Version: 1.0
-Author: Mertcan
-*/
-
-// Enqueue scripts and styles
-function enqueue_tile_calculator_scripts()
-{
-    // Enqueue jQuery if not already enqueued
-    if (!wp_script_is('jquery', 'enqueued')) {
-        wp_enqueue_script('jquery');
-    }
-
-    // Enqueue custom script with localization
-    wp_enqueue_script('tile-calculator-script', plugin_dir_url(__FILE__) . 'tile-calculator.js', array('jquery'), '1.0', true);
-
-    // Localize script to pass data to the script
-    wp_localize_script('tile-calculator-script', 'wpc_vars', array(
-        'sizemm' => esc_attr(get_option('wpc_variation_slug')), // Replace with the actual option name of the slug
-    ));
-}
-
-add_action('wp_enqueue_scripts', 'enqueue_tile_calculator_scripts');
-
 // Shortcode function
 function tile_calculator_shortcode()
 {
@@ -55,6 +28,7 @@ function tile_calculator_shortcode()
 
         // Function to update the calculation based on inputs
         function updateCalculation() {
+            var wpc_vars = <?php echo json_encode(array('sizemm' => html_entity_decode(esc_attr(get_option('wpc_variation_slug'))))); ?>;
             var squareMeterValue = parseFloat(jQuery('#squareMeterInput').val());
             var tileSize = parseFloat(jQuery('input[name="' + wpc_vars.sizemm + '"]:checked').val());
 
@@ -70,6 +44,3 @@ function tile_calculator_shortcode()
     <?php
     return ob_get_clean(); // End and clean the output buffer
 }
-
-add_shortcode('tile_calculator', 'tile_calculator_shortcode');
-?>
